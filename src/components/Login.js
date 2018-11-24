ada4af6e33f265b352b7c8ed81167d66273717a1
Eric Ung <ungericwei@gmail.com>
2018-11-24 20:07:44 +0000

    Login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,16 +4,32 @@ import { connect } from 'react-redux';
 class Login extends Component {
   render() {
     const { users, usersID } = this.props;
+    const divStyle = {
+        width: '50%',
+        height: '50%',
+        margin: 'auto',
+      };
     return(
-      <div>
-        <ul>
-          {usersID.map((user) => (
-            <li key={users[user].name}>
-              {users[user].name}
-            </li>  
-          ))}
-        </ul>
+      <div style={divStyle}>
+        <h>Login</h>
+        <p>
+        <select>
+          <option value="" disabled selected>Select your option</option>
+            {usersID.map((user) => (
+              <option key={users[user].name}>
+                {users[user].name}
+              </option>  
+            ))}
+          </select>
+        </p>
+        <p>
+          <input id="password" placeholder="Enter Password" />
+        </p>
+        <p>
+          <button type="submit">Submit</button>
+        </p>
       </div>
+
     )
   }
 }
